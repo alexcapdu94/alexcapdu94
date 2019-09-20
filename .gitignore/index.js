21389d4e03584474884f68bc8567fcd8cf69fedd
alexcapdu94 <55597846+alexcapdu94@users.noreply.github.com>
2019-09-20 19:34:14 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+let prefix = "!"
+
+client.on("ready", () => {
+    console.log("Le bot est démaré")
+})
+
+client.login('NjE4MzM3MzgwMzY5NDMyNTc2.XXTxew.R0DQGXHWn1GPGzfgLs0ppELv15g')
